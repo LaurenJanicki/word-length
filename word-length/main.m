448f6a1f2437a0b15ba67771f1352c8d78f9195e
Lauren Janicki <lauren.a.janicki@gmail.com>
2018-04-18 14:09:37 -0700

    Completed exercise from chapter 3 of Learn Objective-C on the Mac

--- word-length/main.m
+++ word-length/main.m
@@ -8,10 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+int main(int argc, const char * argv[])
+{
+    if (argc == 1)
+    {
+        NSLog(@"You need to provide a file name");
+        return 1;
     }
-    return 0;
+    
+    FILE *wordFile = fopen(argv[1], "r");
+    char word[100];
+    
+    while (fgets(word, 100, wordFile))
+    {
+        word[strlen(word) -1] = '\0';
+        NSLog(@"%s is %lu characters long", word, strlen(word));
+    }
+    
+    fclose(wordFile);
+    return (0);
 }
